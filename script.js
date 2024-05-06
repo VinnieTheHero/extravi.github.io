49972a7f253cb4bcdb175148e1af17526ea3d966
Extravi <98912029+Extravi@users.noreply.github.com>
2024-05-05 21:53:24 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-const apiUrl = 'https://api.github.com/repos/Extravi/Installer/releases';
+const apiUrl1 = 'https://api.github.com/repos/Extravi/Installer/releases';
+const apiUrl2 = 'https://api.github.com/repos/Extravi/Bloxshade/releases';
+const apiUrl3 = 'https://api.github.com/repos/Extravi/bloxshade-args/releases';
+
 async function fetchData(url) {
     const response = await fetch(url);
     if (!response.ok) {
@@ -6,16 +9,23 @@ async function fetchData(url) {
     }
     return await response.json();
 }
+
 async function calculateTotalDownloadCount() {
     try {
-        const releases = await fetchData(apiUrl);
+        const [releases1, releases2, releases3] = await Promise.all([
+            fetchData(apiUrl1),
+            fetchData(apiUrl2),
+            fetchData(apiUrl3)
+        ]);
+
         let totalDownloadCount = 0;
-        for (const release of releases) {
+        for (const release of releases1.concat(releases2, releases3)) {
             totalDownloadCount += release.assets.reduce(
                 (acc, asset) => acc + asset.download_count,
                 0
             );
         }
+
         const formattedTotal = `- ${totalDownloadCount.toLocaleString()} downloads`;
         const downloadsCountElement = document.querySelector('.downloads-count');
         downloadsCountElement.textContent = formattedTotal;
@@ -23,4 +33,5 @@ async function calculateTotalDownloadCount() {
         console.error('Error:', error);
     }
 }
+
 calculateTotalDownloadCount();
